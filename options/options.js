45697e0993b27d5a005b8d22ebf87673d78188be
Crowdbotics <team@crowdbotics.com>
2024-01-23 16:25:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "altria-internal-lea-45484",
+  url: "https://altria-internal-lea-45484.botics.co",
+  api: "https://altria-internal-lea-45484.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
